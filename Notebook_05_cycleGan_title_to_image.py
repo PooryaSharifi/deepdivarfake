faccb13f0a925a452774c99303675e3d4210a425
Poorya Sharifi <Po00oryaSharifi@gmail.com>
2020-04-22 12:00:23 +0430

    Create Notebook_05_cycleGan_title_to_image.py

---
+++ Notebook_05_cycleGan_title_to_image.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+'''
+bunch of images from divar <-> bunch of commercial images from google search
+  -> train cycle gan to filter commercial images into used blurry images
+with title search in google search get first_image then output gen1(first_image) 
+'''
